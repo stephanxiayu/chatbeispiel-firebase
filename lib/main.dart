3ee6677cd6b174a6777b1d04c769b871ca7985dd
s.granzow <s.granzow@planhero.de>
2022-04-15 20:34:23 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:chat_lernapp/screens/login_screen.dart';
 import 'package:chat_lernapp/screens/registration_screen.dart';
 import 'package:chat_lernapp/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(const FlashChat());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   const FlashChat({Key? key}) : super(key: key);
