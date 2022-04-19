8fe266d49591f0c12423a49484d00ad841187084
s.granzow <s.granzow@planhero.de>
2022-04-19 09:01:42 +0200

    Changes to be committed: 	modified:   lib/screens/login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -23,58 +23,60 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/log1.png'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/log1.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: textFieldDeko.copyWith(hintText: "Email")),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: textFieldDeko.copyWith(hintText: "Passwort")),
-            const SizedBox(
-              height: 24.0,
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-                title: "Login",
-                color: Colors.amber,
-                onPressed: () async {
-                  try {
-                    final user = await _auth.signInWithEmailAndPassword(
-                        email: email!, password: password!);
-                    if (user != null) {
-                      Navigator.pushNamed(context, ChatScreen.id);
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: textFieldDeko.copyWith(hintText: "Email")),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: textFieldDeko.copyWith(hintText: "Passwort")),
+              const SizedBox(
+                height: 24.0,
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                  title: "Login",
+                  color: Colors.amber,
+                  onPressed: () async {
+                    try {
+                      final user = await _auth.signInWithEmailAndPassword(
+                          email: email!, password: password!);
+                      if (user != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } catch (e) {
+                      print(e.toString());
                     }
-                  } catch (e) {
-                    print(e.toString());
-                  }
-                }),
-          ],
+                  }),
+            ],
+          ),
         ),
       ),
     );
